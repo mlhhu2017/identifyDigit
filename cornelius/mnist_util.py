f1b69650d59b2cf91b7143ee4a8456ea52a54eba
yesArjan <yesArjanxbl@gmail.com>
2017-10-31 15:43:29 +0100

    added funtions to mnist_util to data for a specific number
    function to get an array of images for a specific number
    function to get an array with arrays of images for all numbers

--- cornelius/mnist_util.py
+++ cornelius/mnist_util.py
@@ -5,7 +5,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from random import randint
-#get_ipython().magic('matplotlib inline')
 
 # 1. install MNIST from the command line with 'pip install python-mnist'
 # 2. download the data from http://yann.lecun.com/exdb/mnist/
@@ -17,10 +16,10 @@ from random import randint
 #   path            optional, path to the mnist files (default=='data')
 #
 # outputs:
-#   train           ndarray with shape (60000,784), trainings images
-#   train_labels    ndarray with shape (60000,),    training labels
-#   test            ndarray with shape (10000,784), test images
-#   test_labels     ndarray with shape (10000,),    test labels
+#   train           2d-array with shape (60000,784), training images
+#   train_labels    1d-array with shape (60000,),    training labels
+#   test            2d-array with shape (10000,784), test images
+#   test_labels     1d-array with shape (10000,),    test labels
 #
 def get_np_array(path='data'):
     mndata = MNIST(path)
@@ -31,7 +30,7 @@ def get_np_array(path='data'):
 # show_a_num plots a single picture
 #
 # inputs:
-#   num    takes a (784,) shape ndarray (a row)
+#   num    takes 1d-array with shape (784,) containing a single image
 #
 # outputs:
 #   img    matplotlib image
@@ -44,7 +43,7 @@ def show_a_num(num):
 # show_nums plots multiple pictures in a "grid"
 #
 # inputs:
-#   data      takes a (n,784) shape ndarray (multiple rows)
+#   data      takes 2d-array with shape (n,784) containing images
 #   square    optional, boolean, if 'True' forces a square output image (default==False)
 #   xsize     optional, specifies output image length in inches (default==15)
 #   ysize     optional, specifies output image height in inches (default==15)
@@ -72,3 +71,29 @@ def show_nums(data,square=False,xsize=15,ysize=15):
     plt.figure(figsize=(xsize,ysize))
     img = plt.imshow(data, cmap='gray')
     return img
+
+# get_one_num: creates 2d-array containing only images of a single number
+#
+# inputs:
+#   data      takes 2d-array with shape (n,784) containing the images
+#   labels    takes 1d-array with shape (n,)    containing the labels
+#   num       the number you want to filter
+#
+# outputs:
+#   arr       2d-array only containing a sinlge number
+#
+def get_one_num(data,labels,num):
+    return np.array([data[i] for i in range(len(data)) if labels[i] == num])
+
+# get_all_nums: creates a 1d-array containing 2d-arrays for every number
+#              ex. arr[0] = 2d-array containing all images of the number 0
+#
+# inputs:
+#   data      takes 2d-array with shape (n,784) containing the images
+#   labels    takes 1d-array with shape (n,)    containing the labels
+#
+# outputs:
+#   arr       1d-array containing 2d-arrays for every number
+#
+def get_all_nums(data,labels):
+    return np.array([get_one_num(data,labels,i) for i in range(10)])
