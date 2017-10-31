d7d839e63f4d23b50f4d15b3498ddf806cc27879
yesArjan <yesArjanxbl@gmail.com>
2017-10-31 12:26:39 +0100

    mnist_util added
    functions to import the mnist dataset to ndarrays, show plot of single
    and multiple numbers

--- cornelius_crijnen/mnist_util.py
+++ cornelius_crijnen/mnist_util.py
@@ -1,69 +1,74 @@
-
 # coding: utf-8
-
-# In[1]:
-
-
 from mnist import MNIST
+import math
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from random import randint
+#get_ipython().magic('matplotlib inline')
 
 # 1. install MNIST from the command line with 'pip install python-mnist'
 # 2. download the data from http://yann.lecun.com/exdb/mnist/
-# 3. extract the .gz files and rename the files the '.' to '-'
-# Takes a path to data as argument. Looks for the data in the folder 'data/' if called without an argument.
-# Returns np arrays of trainings_images, trainings_labels, test_images, and test_labels in that order.
+# 3. extract the .gz files and rename '.' to '-' in the file names
+#
+# get_np_array converts mnist images to ndarrays
+#
+# inputs:
+#   path            optional, path to the mnist files (default=='data')
+#
+# outputs:
+#   train           ndarray with shape (60000,784), trainings images
+#   train_labels    ndarray with shape (60000,),    training labels
+#   test            ndarray with shape (10000,784), test images
+#   test_labels     ndarray with shape (10000,),    test labels
+#
 def get_np_array(path='data'):
     mndata = MNIST(path)
     train, train_labels = mndata.load_training()
-    test, test_labels = mndata.load_training()
+    test, test_labels = mndata.load_testing()
     return np.array(train), np.array(train_labels), np.array(test), np.array(test_labels)
 
-train, train_labels, test, test_labels = get_np()
-
-
-# In[2]:
-
-
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-get_ipython().magic('matplotlib inline')
-
-# shows image of a single number
-# needs a row with length 784 as argument
+# show_a_num plots a single picture
+#
+# inputs:
+#   num    takes a (784,) shape ndarray (a row)
+#
+# outputs:
+#   img    matplotlib image
+#
 def show_a_num(num):
     pixels = num.reshape((28,28))
-    #plt.title("Label: {label}".format(label=label))
-    plt.imshow(pixels, cmap='gray')
+    img = plt.imshow(pixels, cmap='gray')
+    return img
 
-
-# In[3]:
-
-
-# shows images of multiple numbers in a "grid"
-# need a 2d np array with rows of length 784 as argument
-def show_nums(data):
-    factors = lambda n: set([i for i in range(1, int(n**0.5) + 1) if n % i == 0])
-    fac = factors(len(data))
-    nrow = max(list(fac))
+# show_nums plots multiple pictures in a "grid"
+#
+# inputs:
+#   data      takes a (n,784) shape ndarray (multiple rows)
+#   square    optional, boolean, if 'True' forces a square output image (default==False)
+#   xsize     optional, specifies output image length in inches (default==15)
+#   ysize     optional, specifies output image height in inches (default==15)
+#
+# outputs:
+#   img       matplotlib image
+#
+def show_nums(data,square=False,xsize=15,ysize=15):
+    if len(data.shape) == 1:
+        return show_a_num(data)
+    n = len(data)
+    if square:
+        nrow = math.ceil(math.sqrt(n))
+        missing = nrow**2 - n
+        zeros = np.zeros(missing*784)
+        zeros = zeros.reshape(missing,784)
+        data = np.vstack((data,zeros))
+    else:
+        factors = lambda n: set([i for i in range(1, int(n**0.5) + 1) if n % i == 0])
+        fac = factors(n)
+        nrow = max(list(fac))
     data = data.reshape((-1,28,28))
     data = data.reshape((nrow,-1,28,28)).swapaxes(1,2)
     data = data.reshape((nrow*28,-1))
-    plt.figure(figsize=(15,15))
-    plt.imshow(data, cmap='gray')
-
-
-# In[4]:
-
-
-from random import randint
-
-rnd = randint(0,len(train)-1)
-print("showing picture number: {rnd}".format(rnd=rnd))
-show_a_num(train[rnd])
-
-
-# In[63]:
-
-
-test = train[:100]
-show_nums(test)
+    plt.figure(figsize=(xsize,ysize))
+    img = plt.imshow(data, cmap='gray')
+    return img
